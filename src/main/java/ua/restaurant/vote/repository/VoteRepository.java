de83ac63e604b621d7e89a56663e88894c30c6a6
Galushkin Pavel <gpg@ua.fm>
2017-03-25 21:58:38 +0200

    Hateoas in restaurant's poll

--- src/main/java/ua/restaurant/vote/repository/VoteRepository.java
+++ src/main/java/ua/restaurant/vote/repository/VoteRepository.java
@@ -46,7 +46,4 @@ public interface VoteRepository extends JpaRepository<Vote, Integer> {
 
     @Query(name = "getResultTo")
     List<ResultTo> getResultSet(@Param("date") LocalDate date);
-
-    @Query("SELECT v FROM Vote v WHERE v.id>100017")
-    List<Vote> getAll();
 }
